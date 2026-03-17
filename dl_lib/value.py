2bd9172a72b67b5268032981c0166c2d17854372
genipami <pamivladi@gmail.com>
2026-03-17 17:50:53 +0200

    Started value class.

---
+++ dl_lib/value.py
@@ -0,0 +1,29 @@
+class Value():
+    def __init__(self, data, _prev=None):
+        self.data = data
+        self._prev = _prev
+
+    def __str__(self):
+        return f"Value(data={self.data})"
+    
+    def __repr__(self):
+        return (f"Value(data={self.data})")
+
+
+    def __add__(self, other):
+        return Value(self.data + other.data, {self, other})
+    
+    def __mul__(self, other):
+        return Value(self.data * other.data, {self, other})
+    
+def main() -> None:
+    x = Value(2.0)
+    y = Value(-3.0)
+    z = Value(10.0)
+    result = x * y + z
+    print(result._prev)
+
+
+
+if __name__ == "__main__":
+    main()
