6e8b3d1cbd6a4f7a6f0cbe3fb8f2e5f9cdf1b11c
genipami <pamivladi@gmail.com>
2026-03-20 13:32:08 +0200

    Added task 6 and 7.

--- dl_lib/value.py
+++ dl_lib/value.py
@@ -1,7 +1,11 @@
+import graphviz
+
 class Value():
-    def __init__(self, data, _prev=None):
+    def __init__(self, data, _prev=None, _op = None):
         self.data = data
         self._prev = _prev
+        self._op = _op
+
 
     def __str__(self):
         return f"Value(data={self.data})"
@@ -11,19 +15,60 @@ class Value():
 
 
     def __add__(self, other):
-        return Value(self.data + other.data, {self, other})
+        return Value(self.data + other.data, {self, other}, "+")
     
     def __mul__(self, other):
-        return Value(self.data * other.data, {self, other})
+        return Value(self.data * other.data, {self, other}, "*")
     
-# def main() -> None:
-#     x = Value(2.0)
-#     y = Value(-3.0)
-#     z = Value(10.0)
-#     result = x * y + z
-#     print(result._prev)
+def trace(input):
+        queue = [input]
+        nodes = set()
+        edges = set()
 
+        while len(queue) > 0:
+            curr = queue.pop(0)
+            nodes.add(curr)
 
+            if curr._prev is None:
+                continue
+            
+            for prev in curr._prev:
+                edge = (prev, curr)
+                edges.add(edge)
+                queue.append(prev)
 
-# if __name__ == "__main__":
-#     main()
+        return(nodes, edges)    
+
+def draw_dot(root: Value) -> graphviz.Digraph:
+    dot = graphviz.Digraph(filename='01_result', format='svg', graph_attr={
+                           'rankdir': 'LR'})  # LR = left to right
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        # for any value in the graph, create a rectangular ('record') node
+        dot.node(name=uid, label=f'{{ data: {n.data} }}', shape='record')
+        if n._op:
+            # if this value is a result of some operation, create an "op" node for the operation
+            dot.node(name=uid + n._op, label=n._op)
+            # and connect this node to the node of the operation
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        # connect n1 to the "op" node of n2
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
+def main() -> None:
+    x = Value(2.0)
+    y = Value(-3.0)
+    z = Value(10.0)
+    result = x * y + z
+    
+    # This will create a new directory and store the output file there.
+    # With "view=True" it'll automatically display the saved file.
+    draw_dot(result).render(directory='./graphviz_output', view=True)
+
+if __name__ == "__main__":
+    main()    
--- tests/test_dl_lib/test_value.py
+++ tests/test_dl_lib/test_value.py
@@ -1,13 +1,14 @@
 import unittest
-from unittest.mock import patch, call
-from dl_lib.value import Value
+from unittest.mock import patch
+from dl_lib.value import Value, trace
 
 class TestInit(unittest.TestCase):
-    def test_when_creating_with_data_5_and_without_prev_then_prev_is_none(self):
+    def test_when_creating_with_data_5_and_without_prev_then_prev_and_op_are_none(self):
         #Arrange
         data_to_pass = 5
         expected_data = 5
         expected_prev = None
+        expected_op = None
         
         #Act
         actual = Value(data_to_pass)
@@ -15,14 +16,16 @@ class TestInit(unittest.TestCase):
         #Assert
         self.assertEqual(expected_data, actual.data)
         self.assertEqual(expected_prev, actual._prev)
+        self.assertEqual(expected_op, actual._op)
 
-    def test_when_creating_with_data_3_and_with_prev_with_data_4_then_data_is_3_and_prev_has_data_4(self):
+    def test_when_creating_with_data_3_and_with_prev_with_data_4_then_data_is_3_and_prev_has_data_4_and_op_is_None(self):
         #Arrange
         data_to_pass = 3
         prev_to_pass = Value(4)
 
         expected_data = data_to_pass
         expected_prev = prev_to_pass
+        expected_op = None
 
         #Act
         actual = Value(data_to_pass, prev_to_pass)
@@ -30,6 +33,7 @@ class TestInit(unittest.TestCase):
         #Assert
         self.assertEqual(expected_data, actual.data)
         self.assertEqual(expected_prev, actual._prev)
+        self.assertEqual(expected_op, actual._op)
 
 class TestStr(unittest.TestCase):
     def test_when_passing_value_with_data_5_string_representation_is_as_expected(self):
@@ -58,11 +62,11 @@ class TestRepr(unittest.TestCase):
         self.assertEqual(expected, actual)
 
 class TestAdd(unittest.TestCase):
-    def test_when_adding_value_with_data_2_and_value_with_data_3_then_result_is_value_with_data_5_and_prev_includes_the_values_2_and_3(self):
+    def test_when_adding_value_with_data_2_and_value_with_data_3_then_result_is_value_with_data_5_and_prev_includes_the_values_2_and_3_and_op_is_plus(self):
         #Arrange
         two = Value(2)
         three = Value(3)
-        expected = Value(5, {two, three})
+        expected = Value(5, {two, three}, "+") 
 
         #Act
         actual = two + three 
@@ -71,11 +75,11 @@ class TestAdd(unittest.TestCase):
         self.assertEqual(expected, actual)
 
 class TestAdd(unittest.TestCase):
-    def test_when_multiplying_value_with_data_2_and_value_with_data_3_then_result_is_value_with_data_6_and_prev_includes_the_values_2_and_3(self):
+    def test_when_multiplying_value_with_data_2_and_value_with_data_3_then_result_is_value_with_data_6_and_prev_includes_the_values_2_and_3_and_op_is_mult(self):
         #Arrange
         two = Value(2)
         three = Value(3)
-        expected = Value(6, {two, three})
+        expected = Value(6, {two, three}, "*")
 
         #Act
         actual = two * three 
@@ -83,4 +87,52 @@ class TestAdd(unittest.TestCase):
         #Assert
         self.assertEqual(expected.data, actual.data)
         self.assertEqual(expected._prev, actual._prev)
-    
+
+class TestTrace(unittest.TestCase):
+    def test_when_calling_on_new_value_then_there_is_no_trace(self):
+        #Arrange
+        value_to_pass = Value(5)
+        expected_nodes = {value_to_pass}
+        expected_edges = set()
+
+        #Act
+        actual_nodes, actual_edges = trace(value_to_pass)
+
+        # Assert
+        self.assertEqual(expected_nodes, actual_nodes)
+        self.assertEqual(expected_edges, actual_edges)    
+
+    def test_when_calling_on_the_sum_of_2_and_3_values_then_there_is_trace_with_nodes_2_and_3_and_correct_edges(self):
+        #Arrange
+        two = Value(2)
+        three = Value(3)
+        value_to_pass = two+three
+        
+        expected_nodes = {two, three, value_to_pass}
+        
+        expected_edges = {(two, value_to_pass), (three, value_to_pass)}
+
+        #Act
+        actual_nodes, actual_edges = trace(value_to_pass)
+
+        # Assert
+        self.assertCountEqual(expected_nodes, actual_nodes)
+        self.assertCountEqual(expected_edges, actual_edges)   
+
+    def test_when_doing_multiple_opetarions(self):
+        #Arrange
+        x = Value(2.0)
+        y = Value(-3.0)
+        z = Value(10.0)
+        x_times_y = x*y
+        result = x_times_y+z
+
+        expected_nodes = {x, y, z, x_times_y, result}
+        expected_edges = {(x, x_times_y), (y, x_times_y), (z, result), (x_times_y, result)}
+        
+        #Act
+        actual_nodes, actual_edges = trace(result)
+
+        #Assert
+        self.assertCountEqual(expected_nodes, actual_nodes)
+        self.assertCountEqual(expected_edges, actual_edges)
